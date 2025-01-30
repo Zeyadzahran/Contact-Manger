a807633a9a16a918df395b741a0e2fb9de730717
Zeyad Zahran <155586428+Zeyadzahran@users.noreply.github.com>
2025-01-30 03:34:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,67 +2,64 @@
 #include "ContactBook.h"
 #include "Person.h"
 #include "Address.h"
+
 using namespace std;
 
 bool isNumeric(const string &str);
 void addContact(ContactBook &arr);
-void updateContact(ContactBook &arr , int &idx);
+void updateContact(ContactBook &arr, const int &idx);
 void startMenu();
+
 int main() {
-
-
     string username = "Zeyad";
     int password = 1111;
-
     string s;
-    int pass;
+    int pass, option;
+
     do {
         startMenu();
         cin >> option;
-    cout << "Enter User Name\n";
-    cin >> s;
-    cout << "Enter password\n";
-    cin >> pass;
 
-    if (s != username || pass != password)
-    {
-        do
-        {
-            cout << "User name or password inValied\n";
-            cout << "Enter User Name\n";
+        cout << "Enter User Name\n";
+        cin >> s;
+        cout << "Enter password\n";
+        cin >> pass;
+
+        while (s != username || pass != password) {
+            cout << "User name or password invalid. Try again.\n";
+            cout << "Enter User Name: ";
             cin >> s;
-            cout << "Enter password\n";
+            cout << "Enter password: ";
             cin >> pass;
-        } while (s != username || pass != password);
-    }
+        }
 
-
-    ContactBook contactBook;
-    int option;
+        ContactBook contactBook;
 
         switch (option) {
             case 1:
                 addContact(contactBook);
-            break;
-
-            case 2: {
-                cout << "The Contact That you Delete:\n";
-                contactBook.printSingleElement(contactBook.getSize() - 1);
-                Person p = contactBook.pop();
                 break;
-            }
+
+            case 2:
+                if (contactBook.getSize() > 0) {
+                    cout << "The Contact That You Deleted:\n";
+                    contactBook.printSingleElement(contactBook.getSize() - 1);
+                    contactBook.pop();
+                } else {
+                    cout << "No contacts to delete.\n";
+                }
+                break;
 
             case 3: {
                 int idx;
                 cout << "Enter index to remove: ";
                 cin >> idx;
-                int n = contactBook.getSize();
-                if (idx >= n || idx < 0) {
+                if (idx >= contactBook.getSize() || idx < 0) {
                     cout << "Invalid index\n";
                 } else {
-                    cout << "The Element That you  Delete:\n";
+                    cout << "The Element That You Deleted:\n";
                     contactBook.printSingleElement(idx);
-                    Person p = contactBook.deleteAtIndex(idx);
+                    contactBook.deleteAtIndex(idx);
                 }
                 break;
             }
@@ -80,7 +77,6 @@ int main() {
                 break;
             }
 
-
             case 5: {
                 string lastName;
                 cout << "Enter last name to search: ";
@@ -92,15 +88,13 @@ int main() {
                     contactBook.printSingleElement(res);
                 }
                 break;
-
             }
 
             case 6: {
                 int idx;
                 cout << "Enter index to get contact: ";
                 cin >> idx;
-                int n = contactBook.getSize();
-                if (idx >= n || idx < 0) {
+                if (idx >= contactBook.getSize() || idx < 0) {
                     cout << "Invalid index\n";
                 } else {
                     contactBook.printSingleElement(idx);
@@ -108,60 +102,60 @@ int main() {
                 break;
             }
 
-
             case 7: {
                 string lastName;
-                cout << "Enter Last Name For The Contact You Want To Update : ";
+                cout << "Enter Last Name For The Contact You Want To Update: ";
                 cin >> lastName;
                 int idx = contactBook.searchWithLastName(lastName);
                 if (idx == -1) {
                     cout << "Not Found\n";
                 } else {
-                    updateContact(contactBook , idx);
+                    updateContact(contactBook, idx);
                 }
-
                 break;
             }
 
             case 8:
                 contactBook.print();
-            break;
-            case 9: {
+                break;
+
+            case 9:
                 contactBook.printFav();
                 break;
-            }
-            case 10 : {
-                cout<<"DO YOU WANT TO DELETE CONTACT BOOK !\n";
-                char ans ; cin >> ans  ;
-                ans = tolower(ans);
-                if(ans == 'y') {
+
+            case 10: {
+                cout << "DO YOU WANT TO DELETE CONTACT BOOK? (y/n): ";
+                char ans;
+                cin >> ans;
+                ans = static_cast<char>(tolower(ans));
+                if (ans == 'y') {
                     contactBook.clearBook();
-                    cout<<"Contact Book Deleted Successfully :)\n";
-                }
-                else {
-                    cout<<"Don't worry, No Contact Has been Deleted\nGoodBye";
+                    cout << "Contact Book Deleted Successfully :)\n";
+                } else {
+                    cout << "Don't worry, No Contact Has been Deleted\nGoodbye!\n";
                 }
                 break;
             }
-               case 11: {
+
+            case 11:
                 cout << "Saving contacts and exiting.\n";
                 contactBook.saveToFile("contacts.txt");
                 cout << "Goodbye!\n";
                 break;
-            }
+
             case 12: {
-                    cout << "Loading contacts from file will overwrite current contacts.\n";
-                    cout << "Are you sure you want to continue? (y/n): ";
-                    char confirm;
-                    cin >> confirm;
-                    if (tolower(confirm) == 'y') {
-                        contactBook.loadFromFile("contacts.txt");
-                        cout << "Contacts loaded from file successfully.\n";
-                    } else {
-                        cout << "Load Operation Canceled.\n";
-                    }
-                    break;
+                cout << "Loading contacts from file will overwrite current contacts.\n";
+                cout << "Are you sure you want to continue? (y/n): ";
+                char confirm;
+                cin >> confirm;
+                if (tolower(confirm) == 'y') {
+                    contactBook.loadFromFile("contacts.txt");
+                    cout << "Contacts loaded from file successfully.\n";
+                } else {
+                    cout << "Load Operation Canceled.\n";
                 }
+                break;
+            }
 
             case 13:
                 cout << "Goodbye!\n";
@@ -174,35 +168,40 @@ int main() {
 
     return 0;
 }
-bool isNumeric(const string &str)
-{
 
-    for (char c : str)
-    {
-        if (!isdigit(c))
-        {
+bool isNumeric(const string &str) {
+    for (char c : str) {
+        if (!isdigit(c)) {
             return false;
         }
     }
     return true;
 }
+
 void addContact(ContactBook &arr) {
     string fname, sname, number, country, city, street, homeNum, classification;
     bool fav;
 
-    cout << "Enter First Name: "; cin >> fname;
-    cout << "Enter Last Name: "; cin >> sname;
+    cout << "Enter First Name: ";
+    cin >> fname;
+    cout << "Enter Last Name: ";
+    cin >> sname;
     cout << "Enter Phone Number: ";
     while (true) {
         cin >> number;
         if (isNumeric(number)) break;
         cout << "Invalid input. Please enter only numbers.\n";
     }
-    cout << "Enter classification (Friend , work , family ,other) : "; cin >> classification;
-    cout<<"Is it Fav yes[1] no [0] "; cin>>fav;
-    cout << "Enter Country: "; cin >> country;
-    cout << "Enter City: "; cin >> city;
-    cout << "Enter Street: "; cin >> street;
+    cout << "Enter classification (Friend, Work, Family, Other): ";
+    cin >> classification;
+    cout << "Is it Favorite? yes[1] no [0]: ";
+    cin >> fav;
+    cout << "Enter Country: ";
+    cin >> country;
+    cout << "Enter City: ";
+    cin >> city;
+    cout << "Enter Street: ";
+    cin >> street;
     cout << "Enter Home Number: ";
     while (true) {
         cin >> homeNum;
@@ -211,27 +210,35 @@ void addContact(ContactBook &arr) {
     }
 
     Address a(country, city, street, homeNum);
-    Person p(fname, sname, number, classification, a,fav);
+    Person p(fname, sname, number, classification, a, fav);
     arr.addNew(p);
 }
-void updateContact(ContactBook &arr , int &idx) {
-    string fname, sname, number, country, city, street, homeNum, classification;
-    bool fav ;
-    cout<<"Enter The New Details for the Contact To Be Updated\n";
 
-    cout << "Enter First Name: "; cin >> fname;
-    cout << "Enter Last Name: "; cin >> sname;
+void updateContact(ContactBook &arr, const int &idx) {
+    string fname, sname, number, country, city, street, homeNum, classification;
+    bool fav;
+
+    cout << "Enter The New Details for the Contact to Be Updated\n";
+    cout << "Enter First Name: ";
+    cin >> fname;
+    cout << "Enter Last Name: ";
+    cin >> sname;
     cout << "Enter Phone Number: ";
     while (true) {
         cin >> number;
         if (isNumeric(number)) break;
         cout << "Invalid input. Please enter only numbers.\n";
     }
-    cout << "Enter classification (Friend , work , family ,other) "; cin >> classification;
-    cout<<"Is it Fav yes[1] no [0] "; cin>>fav;
-    cout << "Enter Country: "; cin >> country;
-    cout << "Enter City: "; cin >> city;
-    cout << "Enter Street: "; cin >> street;
+    cout << "Enter classification (Friend, Work, Family, Other): ";
+    cin >> classification;
+    cout << "Is it Favorite? yes[1] no [0]: ";
+    cin >> fav;
+    cout << "Enter Country: ";
+    cin >> country;
+    cout << "Enter City: ";
+    cin >> city;
+    cout << "Enter Street: ";
+    cin >> street;
     cout << "Enter Home Number: ";
     while (true) {
         cin >> homeNum;
@@ -240,9 +247,10 @@ void updateContact(ContactBook &arr , int &idx) {
     }
 
     Address a(country, city, street, homeNum);
-    Person p(fname, sname, number, classification, a,fav);
-    arr.updateContact(idx ,p);
+    Person p(fname, sname, number, classification, a, fav);
+    arr.updateContact(idx, p);
 }
+
 void startMenu() {
     cout << "\nChoose an operation:\n";
     cout << "1 - Add New Contact\n"
@@ -253,10 +261,9 @@ void startMenu() {
          << "6 - View Contact Details\n"
          << "7 - Update Contact\n"
          << "8 - Display All Contacts\n"
-         << "9 - PrintFav\n"
+         << "9 - Print Favorites\n"
          << "10 - Delete Contact Book\n"
          << "11 - Save Contacts To File\n"
-         << "12 - Load Contacts To File\n"
+         << "12 - Load Contacts From File\n"
          << "13 - Exit\n";
 }
-
